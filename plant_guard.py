cbb1802484874b2e462c830fe1d8a89d2785a1d4
afrixs <matej.vargovcik@gmail.com>
2021-06-15 10:41:39 +0200

    simple working solution (user interface for cron jobs and yaml params configuration, stepper motor and pump control) with no sensors and no web

---
+++ plant_guard.py
@@ -0,0 +1,177 @@
+import RPi.GPIO as GPIO
+import time
+from threading import Lock, Thread
+from pathlib import Path
+
+lock = Lock()
+keyboardListening = False
+keyboardDir = 0
+movementTerminated = False
+rikt = []
+
+PUMP_PIN = 5
+PUMP_REV_PIN = 6
+
+def init(exitOnLoadFailure):
+  GPIO.setmode(GPIO.BCM)
+  initPump()
+  initStepper(exitOnLoadFailure)
+
+def initPump():
+  GPIO.setup(PUMP_PIN, GPIO.OUT)
+  GPIO.output(PUMP_PIN, GPIO.LOW)
+  GPIO.setup(PUMP_REV_PIN, GPIO.OUT)
+  GPIO.output(PUMP_REV_PIN, GPIO.LOW)
+  pass
+
+def startPump():
+  print("pump: 1")
+  GPIO.output(PUMP_PIN, GPIO.HIGH)
+
+def stopPump():
+  print("pump: 0")
+  GPIO.output(PUMP_PIN, GPIO.LOW)
+
+
+STEPPER_PINS = [24, 23, 22, 27]
+STEPS = [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1]]
+currentStep = 0
+currentAngle = 0
+fullRot = 512*8
+
+stepperSleepDur = 0.005
+
+def computeChecksum():
+  return 0xFFFFFFFF - (currentStep + currentAngle)
+
+def saveFilePath(backup):
+  return str(Path.home())+'/plant-guard/save'+('_backup.txt' if backup else '.txt')
+
+def readSavedData(backup):
+  global currentStep
+  global currentAngle
+  try:
+    with open(saveFilePath(backup)) as file:
+      currentStep = int(file.readline())
+      currentAngle = int(file.readline())
+      checksum = int(file.readline())
+      if checksum != computeChecksum():
+        raise
+  except:
+    return False
+  return True
+
+def writeSavedData(backup):
+  with open(saveFilePath(backup), 'w') as file:
+    file.writelines([str(currentStep)+'\n', str(currentAngle)+'\n', str(computeChecksum())+'\n'])
+
+def initStepper(exitOnLoadFailure):
+  if not readSavedData(False) and not readSavedData(True):
+    if exitOnLoadFailure:
+      print("ERROR: No saved state file found")
+      exit(-1)
+    else:
+      print("WARNING: No saved state file found. This is ok if running for the first time")
+
+  for p in STEPPER_PINS:
+    GPIO.setup(p, GPIO.OUT)
+  updateStepper()
+
+def updateStepper():
+  # print(STEPS[currentStep])
+  writeSavedData(False)
+  writeSavedData(True)
+  for (p, v) in zip(STEPPER_PINS, STEPS[currentStep]):
+    GPIO.output(p, GPIO.HIGH if v == 1 else GPIO.LOW)
+
+def degAngle(deg):
+  return round(deg*512.0/45.0)
+
+def rotateSteps(stepsCnt):
+  global currentStep
+  global currentAngle
+  with lock:
+    for _ in range(abs(stepsCnt)):
+      if stepsCnt > 0:
+        currentStep = (currentStep+1)%len(STEPS)
+        currentAngle = (currentAngle+1)%fullRot
+      else:
+        currentStep = (currentStep - 1 + len(STEPS))%len(STEPS)
+        currentAngle = (currentAngle - 1 + fullRot)%fullRot
+      updateStepper()
+      time.sleep(stepperSleepDur)
+      if movementTerminated:
+        break
+
+def rotateTo(angle):
+  global currentAngle
+  with lock:
+    stepsCnt = angle - currentAngle
+  while stepsCnt > fullRot/2:
+    stepsCnt -= fullRot
+  while stepsCnt < -fullRot/2:
+    stepsCnt += fullRot
+  #don't rotate through 360 degrees because of hose and (future) cables
+  if currentAngle > 0 and angle < 0 and stepsCnt > 0:
+    stepsCnt -= fullRot
+  elif currentAngle < 0 and angle > 0 and stepsCnt < 0:
+    stepsCnt += fullRot
+  
+  rotateSteps(stepsCnt)
+
+def patrol(angle1, angle2):
+  if angle1 == angle2:
+    return
+  while not movementTerminated:
+    rotateTo(angle2)
+    if not movementTerminated:
+      rotateTo(angle1)
+
+def water(angle1, angle2, dur):
+  global movementTerminated
+  rotateTo(angle1)
+  startPump()
+  movementTerminated = False
+  t = Thread(target=patrol, args=(angle1, angle2))
+  t.start()
+  time.sleep(dur)
+  movementTerminated = True
+  t.join()
+  movementTerminated = False
+  stopPump()
+  time.sleep(1.0)
+
+def setKeyboardListening(kl):
+  global keyboardListening
+  global rikt
+  if keyboardListening != kl:
+    keyboardListening = kl
+    if kl:
+      rikt = Thread(target=rotateIfKey)
+      rikt.start()
+    else:
+      rikt.join()
+
+def setKeyboardDir(kd):
+  global keyboardDir
+  keyboardDir = kd
+def getKeyboardDir():
+  return keyboardDir
+
+def getCurrentAngle():
+  return currentAngle
+
+def rotateIfKey():
+  global currentStep
+  global currentAngle
+  while keyboardListening:
+    if keyboardDir != 0:
+      with lock:
+        if keyboardDir > 0:
+          currentStep = (currentStep+1)%len(STEPS)
+          currentAngle = (currentAngle+1)%fullRot
+        else:
+          currentStep = (currentStep - 1 + len(STEPS))%len(STEPS)
+          currentAngle = (currentAngle - 1 + fullRot)%fullRot
+        updateStepper()
+    time.sleep(stepperSleepDur)
---
+++ plant_guard_control.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3
+
+#pip3 install python_crontab pyyaml portalocker
+from plant_guard import *
+from crontab import CronTab, CronSlices
+import yaml
+import portalocker
+from pathlib import Path
+import sys,tty,os,termios
+import getpass
+import re
+# from pynput import keyboard
+
+pumpStartTime = time.time()
+pumpingDuration = 0
+angleFrom = -1
+angleTo = -1
+pumpRunning = False
+
+def on_press(k):
+  global pumpStartTime
+  global pumpingDuration
+  global angleFrom
+  global angleTo
+  global pumpRunning
+  # if key == keyboard.Key.esc:
+  # if k == 'esc':
+  #   if keyboardListening:
+  #     print("Stopping keyboard control. Press Enter to start keyboard control again")
+  #     keyboardListening = False
+  #   else:
+  #     print("Stopping keyboard listening")
+  #     return False
+  # if key == keyboard.Key.enter:
+  if k == 'return':
+    if angleTo == -1 and angleFrom == -1:
+      print("first press 'f' or 't' to set angleFrom or angleTo")
+      return True
+    if pumpingDuration == 0:
+      print("first press and hold 'p' to set pumping duration")
+      return True
+    print("Enter a name of the new job")
+    name = input()
+    try:
+      with open(str(Path.home())+'/plant-guard/jobs.yaml') as file:
+        jobsData = yaml.full_load(file)
+    except:
+      jobsData = {}
+    if jobsData is None:
+      jobsData = {}
+    jobsData[name] = {
+        'angle_from': angleFrom if angleFrom != -1 else angleTo,
+        'angle_to': angleTo if angleTo != -1 else angleFrom,
+        'pump_dur': pumpingDuration
+      }
+    with open(str(Path.home())+'/plant-guard/jobs.yaml', 'w') as file:
+      yaml.dump(jobsData, file)
+    
+    print("Enter schedule\nminute hour day_of_month month day_of_week\nexample (every friday at 6AM and 6PM): * 6,18 * * Fri)")
+    schedule = input()
+    while not CronSlices.is_valid(schedule):
+      print("Invalid schedule")
+      schedule = input()
+    cron = CronTab(user=getpass.getuser())
+    job = cron.new(command='python3 ~/plant-guard/plant_guard_job.py '+name, comment='plant-guard-'+name)
+    job.setall(schedule)
+    cron.write()
+    print("Added job "+name+"  "+schedule+"  "+str(jobsData[name]))
+
+  # try:
+  #   k = key.char  # single-char keys
+  # except:
+  #   k = key.name  # other keys
+  if k == 'l':
+    print("Plant-guard jobs:")
+    cron = CronTab(user=getpass.getuser())
+    try:
+      with open(str(Path.home())+'/plant-guard/jobs.yaml') as file:
+        jobsData = yaml.full_load(file)
+    except:
+      jobsData = {}
+    if jobsData is None:
+      jobsData = {}
+    for job in cron.find_comment(re.compile("plant-guard-*")):
+      name = job.comment[len("plant-guard-"):]
+      print(name+"  "+str(job.slices)+"  "+(str(jobsData[name]) if name in jobsData else "No data!"))
+  if k == 'd':
+    print("Enter the name of the job to be deleted")
+    name = input()
+    cron = CronTab(user=getpass.getuser())
+    if cron.remove_all(comment="plant-guard-"+name) == 0:
+      print("No job found")
+    else:
+      print("Deleted job")
+    cron.write()
+
+    try:
+      with open(str(Path.home())+'/plant-guard/jobs.yaml') as file:
+        jobsData = yaml.full_load(file)
+    except:
+      jobsData = {}
+    if jobsData is None:
+      jobsData = {}
+    if name in jobsData:
+      del jobsData[name]
+      with open(str(Path.home())+'/plant-guard/jobs.yaml', 'w') as file:
+        yaml.dump(jobsData, file)
+      print("Deleted data")
+    else:
+      print("No data found")
+  if k == 'x':
+    cron = CronTab(user=getpass.getuser())
+    if cron.remove_all(comment=re.compile("plant-guard-*")) == 0:
+      print("No job found")
+    else:
+      print("Deleted all jobs")
+    cron.write()
+    with open(str(Path.home())+'/plant-guard/jobs.yaml', 'w') as file:
+      pass
+    print("Deleted job data")
+
+  if k == 'left':
+    setKeyboardDir(0 if getKeyboardDir() == 1 else 1)
+  if k == 'right':
+    setKeyboardDir(0 if getKeyboardDir() == -1 else -1)
+  if k == 'p':
+    if pumpRunning:
+      pumpRunning = False
+      stopPump()
+      pumpingDuration = time.time() - pumpStartTime
+      print("Pumping time: " + str(pumpingDuration))
+    else:
+      print("Starting pump")
+      pumpRunning = True
+      startPump()
+      pumpStartTime = time.time()
+
+  if k == 'f':
+    angleFrom = getCurrentAngle()
+    print("Angle from: " + str(angleFrom))
+  if k == 't':
+    angleTo = getCurrentAngle()
+    print("Angle to: " + str(angleTo))
+
+  return True
+
+# def on_release(key):
+#   global pumpingDuration
+#   global pumpRunning
+#   try:
+#     k = key.char  # single-char keys
+#   except:
+#     k = key.name  # other keys
+#   if (k == 'left' and getKeyboardDir() == 1) or (k == 'right' and getKeyboardDir() == -1):
+#     setKeyboardDir(0)
+#     print("Angle: " + str(getCurrentAngle()))
+#   if k == 'p' and pumpRunning:
+#     pumpRunning = False
+#     stopPump()
+#     pumpingDuration = time.time() - pumpStartTime
+#     print("Pumping time: " + str(pumpingDuration))
+#   if key == keyboard.Key.esc:
+#     return False
+#   if k == 'k':
+#     print("Keyboard test: on release")
+
+#   return True
+
+def getkey():
+  old_settings = termios.tcgetattr(sys.stdin)
+  tty.setcbreak(sys.stdin.fileno())
+  try:
+    while True:
+      b = os.read(sys.stdin.fileno(), 3).decode()
+      if len(b) == 3:
+        k = ord(b[2])
+      else:
+        k = ord(b)
+      key_mapping = {
+          127: 'backspace',
+          10: 'return',
+          32: 'space',
+          9: 'tab',
+          27: 'esc',
+          65: 'up',
+          66: 'down',
+          67: 'right',
+          68: 'left'
+      }
+      return key_mapping.get(k, chr(k))
+  finally:
+      termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+# def on_press_dummy(key):
+#   try:
+#     k = key.char  # single-char keys
+#   except:
+#     k = key.name  # other keys
+#   print(k + " pressed")
+#   return True
+
+if __name__ == '__main__':
+  with portalocker.TemporaryFileLock(str(Path.home())+'/plant-guard/.lock', timeout=1800, fail_when_locked=False):
+    init(False)
+
+    # listener = keyboard.Listener(on_release=on_release)
+    # listener.start()
+    setKeyboardListening(True)
+    try:
+      while True:
+        k = getkey()
+        if k == 'esc' or k == 'q':
+          quit()
+        else:
+          on_press(k)
+    except (KeyboardInterrupt, SystemExit):
+      os.system('stty sane')
+      print('stopping.')
+    setKeyboardListening(False)
+    # listener.join()
